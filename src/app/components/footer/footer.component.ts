84781de8a52490658bf22fd8d57b9c9821d3e7fd
te-sa <teresa_saller@freenet.de>
2023-02-12 13:35:38 -0800

    wip

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,18 @@
 import {Component} from '@angular/core'
+import {HeaderComponent} from '../header/header.component'
 
 @Component({
+    providers: [HeaderComponent],
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+    constructor(private headerComponent: HeaderComponent) {
+    }
+
+    public isNightMode(): boolean {
+        console.log(this.headerComponent.isNightMode())
+        return this.headerComponent.isNightMode()
+    }
 }
