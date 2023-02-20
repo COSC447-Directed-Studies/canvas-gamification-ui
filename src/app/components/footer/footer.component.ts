46553819a7ca657ca19f7975dd93927991a4a07f
te-sa <teresa_saller@freenet.de>
2023-02-20 15:22:04 -0800

    removed providers

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,7 @@
 import {Component} from '@angular/core'
-import {HeaderComponent} from '../header/header.component'
 import {NightModeService} from "@app/_services/night-mode.service"
 
 @Component({
-    providers: [HeaderComponent],
     selector: 'app-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['./footer.component.scss']
